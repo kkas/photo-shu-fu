bd7a0945230099dc4354b2473d1d5a3eac4f4165
kkas <kentakikui@gmail.com>
2018-01-04 23:50:37 +0900

    Add animation to the client when sending a picture

--- src/js/client.js
+++ src/js/client.js
@@ -1,96 +1,107 @@
+import anime from 'animejs';
 import { Console, concatStrings } from './myCommon';
 
-const app = function mainApp() {
-  const $mainDiv = $('#main');
-  const $inputImage = $('#image');
-  const $instruction = $('#instruction');
-  const $previewPlaceholder = $('#preview_placeholder');
+const $inputImage = $('#image');
+const $instruction = $('#instruction');
+const $previewPlaceholder = $('#preview_placeholder');
 
-  const removePreviewImage = function removePreviewImage() {
-    Console.log('removePreviewImage called');
-    $previewPlaceholder.empty();
-  };
+const removePreviewImage = function removePreviewImage() {
+  $previewPlaceholder.empty();
+};
 
-  const restoreInstruction = function restoreInstruction() {
-    Console.log('restoreInstruction called');
-    $instruction.show();
-  };
+const restoreInstruction = function restoreInstruction() {
+  $instruction.show();
+};
 
-  // Send a POST request to the server's api and register the picture selected by the client.
-  // TODO: Replace this with the throwing animation.
-  const postImage = function postImage(evt) {
-    evt.preventDefault();
+const animate = function animate(id) {
+  const targetId = concatStrings('#', id);
+  const element = document.getElementById(id);
+  Console.log('targetId:', targetId);
+  Console.log('element:', element);
+  const clientRect = element.getBoundingClientRect();
 
-    const imageSrc = evt.target.src;
+  anime({
+    targets: targetId,
+    duration: 1000,
+    easing: 'easeInQuart',
+    translateY: clientRect.height * -1,
+    complete: function complete() {
+      // Remove the picture from the img element.
+      removePreviewImage();
 
-    // Send a post request
-    $.ajax({
-      url: 'picture',
-      type: 'POST',
-      data: JSON.stringify({
-        data: imageSrc,
-      }),
-      // contentType: false,       // The content type used when sending data to the server.
-      // cache: false,             // To unable request pages to be cached.
-      // processData: false,       // Needs to be set 'false' since I am uploading an image.
-      success: function postData(data) {
-        $mainDiv.append(concatStrings('<p>', 'POST succeeded. data:', data, '</p>'));
-        Console.log('data:', data);
-        // Remove the picture from the img element.
-        removePreviewImage();
-
-        // Restore the icon.
-        restoreInstruction();
-      },
-    });
-  };
-
-  const renderPreviewImage = function renderPreviewImage(evt) {
-    Console.log('renderPreviewImage called. evt: ', evt);
-
-    // Retrieve or create the new Img element.
-    let $newImg = $('#selected_image');
-    if ($newImg.length === 0) {
-      // Create one.
-      $newImg = $("<img id='selected_image'>");
-      // Add an onclick event to send the picture.
-      $newImg.click(postImage);
-    }
-    // Load the selected image.
-    $newImg.attr('src', evt.target.result);
-
-    // Load the selected image as a preview.
-    $previewPlaceholder.append($newImg);
-  };
-
-  const hideInstruction = function hideInstruction() {
-    $instruction.hide();
-  };
-
-  const loadPreviewImage = function loadPreviewImage(evt) {
-    Console.log('loadPreviewImage called. evt: ', evt);
-
-    // Hide the instruction.
-    hideInstruction();
-
-    // Render the preview image.
-    renderPreviewImage(evt);
-  };
-
-  // Set an event when the client selects an image.
-  $inputImage.change(function processSelectedImage(evt) {
-    Console.log('$inputImage change called. evt: ', evt);
-
-    // Retrieve the selected image.
-    if (this.files.length !== 0) {
-      const file = this.files[0];
-      const reader = new FileReader();
-      reader.readAsDataURL(file); // The data will be encoded as base64.
-
-      // Set a callback function to render the selected image when the file is loaded.
-      reader.onload = loadPreviewImage;
-    }
+      // Restore the icon.
+      restoreInstruction();
+    },
   });
 };
 
-$(document).ready(app(this));
+// Send a POST request to the server's api and register the picture selected by the client.
+// TODO: Replace this with the throwing animation.
+const postImage = function postImage(evt) {
+  evt.preventDefault();
+
+  const imageSrc = evt.target.src;
+
+  // Send a post request
+  $.ajax({
+    url: 'picture',
+    type: 'POST',
+    data: JSON.stringify({
+      data: imageSrc,
+    }),
+    success: function postData(data) {
+      Console.log('data:', data);
+    },
+  });
+};
+
+const sendImage = function sendImage(evt) {
+  // Animate the selected picture.
+  animate(evt.target.id);
+
+  postImage(evt);
+};
+
+const renderPreviewImage = function renderPreviewImage(evt) {
+  // Retrieve or create the new Img element.
+  let $newImg = $('#selected_image');
+
+  if ($newImg.length === 0) {
+    // Create one.
+    $newImg = $("<img id='selected_image'>");
+
+    // Add an onclick event to send the picture.
+    $newImg.click(sendImage);
+  }
+  // Load the selected image.
+  $newImg.attr('src', evt.target.result);
+
+  // Load the selected image as a preview.
+  $previewPlaceholder.append($newImg);
+};
+
+const hideInstruction = function hideInstruction() {
+  $instruction.hide();
+};
+
+const loadPreviewImage = function loadPreviewImage(evt) {
+  // Hide the instruction.
+  hideInstruction();
+
+  // Render the preview image.
+  renderPreviewImage(evt);
+};
+
+// Set an event when the client selects an image.
+$inputImage.change(function processSelectedImage() {
+  // Retrieve the selected image.
+  if (this.files.length !== 0) {
+    const file = this.files[0];
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file); // The data will be encoded as base64.
+
+    // Set a callback function to render the selected image when the file is loaded.
+    reader.onload = loadPreviewImage;
+  }
+});
