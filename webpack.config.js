08dce266904a7a99697e5be97462838959f822f1
kkas <kentakikui@gmail.com>
2018-01-07 08:12:22 +0900

    Fix configuration syntax

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,11 @@
 const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const webpack = require('webpack');
 
 module.exports = {
   entry: {
     app: './src/js/app.js',
-    client:'./src/js/client.js',
-    myCommon:'./src/js/myCommon.js',
+    client: './src/js/client.js',
+    myCommon: './src/js/myCommon.js',
   },
   output: {
     filename: '[name].bundle.js',
