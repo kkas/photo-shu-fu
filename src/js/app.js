47a340417e915891c5ffb095229a3bd1b3c4eb72
kkas <kentakikui@gmail.com>
2018-01-07 08:17:32 +0900

    Remove unnecessary wrapup

--- src/js/app.js
+++ src/js/app.js
@@ -1,101 +1,99 @@
 import { Console, concatStrings } from './myCommon';
 import Picture from './Picture';
 
-(function mainApp() {
-  const pictures = [];
+const pictures = [];
 
-  const addImage = (url) => {
-    const picture = new Picture(pictures.length, url);
+const addImage = (url) => {
+  const picture = new Picture(pictures.length, url);
 
-    const mainElem = document.getElementById('main');
-    mainElem.appendChild(picture.element);
+  const mainElem = document.getElementById('main');
+  mainElem.appendChild(picture.element);
 
-    // Start the animation of this picture.
-    picture.animate();
+  // Start the animation of this picture.
+  picture.animate();
 
-    pictures.push(picture);
-  };
+  pictures.push(picture);
+};
 
-  const retrieveImage = (url, callback) => {
-    // Make a GET reqest.
-    const httpRequest = new XMLHttpRequest();
+const retrieveImage = (url, callback) => {
+  // Make a GET reqest.
+  const httpRequest = new XMLHttpRequest();
 
-    // Set a handler to process the response.
-    httpRequest.onreadystatechange = function stateChangeHandler() {
-      try {
-        if (httpRequest.readyState === XMLHttpRequest.DONE) {
-          if (httpRequest.status === 200) {
-            const response = httpRequest.responseText;
-            Console.log('response:', response);
-            callback(response);
-          } else {
-            // Error occured.
-            Console.error('There was a problem with the request.');
-          }
+  // Set a handler to process the response.
+  httpRequest.onreadystatechange = function stateChangeHandler() {
+    try {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          const response = httpRequest.responseText;
+          Console.log('response:', response);
+          callback(response);
+        } else {
+          // Error occured.
+          Console.error('There was a problem with the request.');
         }
-      } catch (e) {
-        Console.error('Caught Exception: ', e.description);
       }
-    };
-
-    // Process the request asynchronously.
-    httpRequest.open('GET', url, true);
-
-    // Send the request
-    httpRequest.send(null);
+    } catch (e) {
+      Console.error('Caught Exception: ', e.description);
+    }
   };
 
-  window.addEventListener('load', () => {
-    // ------------
-    // Websocket
-    // ------------
-    let wsHost = window.location.origin.replace(/^http/, 'ws');
-    const path = '/ws';
-    wsHost += path;
-    const ws = new WebSocket(wsHost);
+  // Process the request asynchronously.
+  httpRequest.open('GET', url, true);
 
-    ws.onopen = () => {
-      Console.log('ws.onopen called.');
-    };
+  // Send the request
+  httpRequest.send(null);
+};
 
-    ws.onmessage = (evt) => {
-      const data = JSON.parse(evt.data);
+window.addEventListener('load', () => {
+  // ------------
+  // Websocket
+  // ------------
+  let wsHost = window.location.origin.replace(/^http/, 'ws');
+  const path = '/ws';
+  wsHost += path;
+  const ws = new WebSocket(wsHost);
 
-      if (data.msg && data.msg === 'PONG') {
-        return;
-      }
+  ws.onopen = () => {
+    Console.log('ws.onopen called.');
+  };
 
-      const url = concatStrings(window.location.origin, '/picture/', data.id);
+  ws.onmessage = (evt) => {
+    const data = JSON.parse(evt.data);
 
-      retrieveImage(url, addImage);
-    };
+    if (data.msg && data.msg === 'PONG') {
+      return;
+    }
 
-    ws.checkConnection = () => {
-      this.sendPING();
-    };
+    const url = concatStrings(window.location.origin, '/picture/', data.id);
 
-    ws.sendPING = () => {
-      ws.send('PING');
-    };
+    retrieveImage(url, addImage);
+  };
 
-    ws.onclose = (close) => {
-      Console.log('ws.onclose() called.');
-      Console.log('close:', close);
-    };
+  ws.checkConnection = () => {
+    this.sendPING();
+  };
 
-    ws.onerror = (evt) => {
-      Console.log('ws.onerror() called.');
-      Console.log('evt:', evt);
-    };
+  ws.sendPING = () => {
+    ws.send('PING');
+  };
 
-    // Periodic PING-PONG messaging to prevent the conncetion terminated by Heroku.
-    // https://devcenter.heroku.com/articles/error-codes#h15-idle-connection
-    const INTERVAL = 10000; // 10sec
-    const keepConnection = () => {
-      if (ws) {
-        ws.sendPING();
-      }
-    };
-    const intervalID = setInterval(keepConnection, INTERVAL);
-  });
-}(this));
+  ws.onclose = (close) => {
+    Console.log('ws.onclose() called.');
+    Console.log('close:', close);
+  };
+
+  ws.onerror = (evt) => {
+    Console.log('ws.onerror() called.');
+    Console.log('evt:', evt);
+  };
+
+  // Periodic PING-PONG messaging to prevent the conncetion terminated by Heroku.
+  // https://devcenter.heroku.com/articles/error-codes#h15-idle-connection
+  const INTERVAL = 10000; // 10sec
+  const keepConnection = () => {
+    if (ws) {
+      ws.sendPING();
+    }
+  };
+  const intervalID = setInterval(keepConnection, INTERVAL);
+});
